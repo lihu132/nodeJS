1b62ed5e4704a939cdd9c5ba718ca13150d01712
lihu132 <lihu132@gmail.com>
2022-05-24 00:18:22 +1000

    readFile

--- 5-fs.js
+++ 5-fs.js
@@ -1,8 +1,24 @@
 const { log } = require('console');
+//1.导入fs模块
 const fs = require('fs');
 
+
+//2. 调用fs.readFile()
+//   参数1：获取文件的存放路径
+//   参数2：获取文件时采用的编码格式，一般默认UTF8
+//   参数3：回调函数，拿到读取失败和成功的结果 err dataStr
+
 fs.readFile('./files/1.txt', 'utf8', function (err, dataStr) {
-    console.log(err);
-    console.log('-----');
-    console.log(dataStr);
+    // //如果读取成功，则err为null
+    // console.log(err);
+    // console.log('-----');
+    // //如果读取失败，则err的值为错误对象，dataStr为undefined
+    // console.log(dataStr);
+
+
+    if (err) {
+        return console.log(`文件读取失败:${err.message}`);
+    }
+
+    console.log(`文件读取成功，内容是:${dataStr}`);
 })
