e7cc705d5fb543dc6313cc085416601e8f400514
lihu132 <lihu132@gmail.com>
2022-05-24 00:06:52 +1000

    readFile

--- 5-fs.js
+++ 5-fs.js
@@ -1,1 +1,8 @@
+const { log } = require('console');
 const fs = require('fs');
+
+fs.readFile('./files/1.txt', 'utf8', function (err, dataStr) {
+    console.log(err);
+    console.log('-----');
+    console.log(dataStr);
+})
