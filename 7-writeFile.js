b51083f09d0f8ecbd7d34cced0f07f6ab9b77231
lihu132 <lihu132@gmail.com>
2022-05-24 22:49:40 +1000

    writeFile

--- 7-writeFile.js
+++ 7-writeFile.js
@@ -6,9 +6,15 @@ const fs = require('fs');
 //   参数1：表示文件的存放路径
 //   参数2：表示写入的内容
 //   参数3：回调函数
-fs.writeFile("f:/files/2.txt", 'abcd', 'utf8', function(err) {
+fs.writeFile("./files/3.txt", 'ok123', 'utf8', function (err) {
     //成功以后，err为null
-    console.log(err);
+    // console.log(err);
 
     //失败，则报错误码
+    if (err) {
+        console.log(`文件写入失败${err.message}`);
+    }
+    else {
+        console.log(('文件写入成功！'));
+    }
 });
