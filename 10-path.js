54feee480d5b89c4035e08dc3e2cfe0014f5ff5c
leo-outside <morinbrit@gmail.com>
2022-05-30 15:12:40 +1000

    path.join

--- 10-path.js
+++ 10-path.js
@@ -1,8 +1,17 @@
 const path = require('path');
 const fs = require('fs');
 
-const pathStr = path.join('/a', '/b/c', '../', '/d', '/e');
+const pathStr = path.join('/a', '/b/c', '../../', './d', '/e');
 console.log(pathStr);
 
 const pathStr2 = path.join(__dirname, './files/1.txt');
 console.log(pathStr2);
+
+
+fs.readFile(path.join(__dirname, './files/1.txt'), 'utf8', function(err, dataStr) {
+    if (err) {
+        return console.log(err.message);
+    }
+
+    console.log(dataStr);
+});
