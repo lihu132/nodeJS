550b4d8700df064682d2f5f20aa35e2d61494bdd
leo-outside <morinbrit@gmail.com>
2022-05-30 15:23:53 +1000

    path.basename

---
+++ 11-path-basename.js
@@ -0,0 +1,9 @@
+const path = require('path');
+
+const fpath = '/a/b/c/index.html';
+
+var fullName = path.basename(fpath);
+console.log(fullName);
+
+var nameWithoutExt = path.basename(fpath, '.html');
+console.log(nameWithoutExt);
